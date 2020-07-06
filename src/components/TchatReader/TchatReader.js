4572b6d7ca76d25f62d6579b7c3bc4b0fcffcdc8
alex <desorbaix@free.fr>
2020-07-07 01:52:13 +0200

    readerMessage only structure

--- src/components/TchatReader/TchatReader.js
+++ src/components/TchatReader/TchatReader.js
@@ -2,14 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TchatReader.module.scss';
 
-const TchatReader = () => (
+export const TchatReaderMessage = (props) => {
+  const isMine = props.message.from === props.nickname ? true : false;
+  return (
+    <div >
+    <div className={styles.TchatReaderMessage} style={{textAlign: (isMine ? 'right' : 'left'), width:'100%' }}>
+        <div className={isMine ? styles.TchatReaderMessageContainer : styles.TchatReaderMyMessageContainer}>
+          {props.message.from}--&gt;{props.message.message}
+        </div>
+      </div>
+    </div>
+  );
+}
+TchatReaderMessage.propTypes = {
+  message: PropTypes.object.isRequired,//impossible avec les proptype TS+++{from:'userNick',message:""}
+  nickname: PropTypes.string.isRequired
+}
+
+
+const TchatReader = (props) => (
   <div className={styles.TchatReader} data-testid="TchatReader">
-    TchatReader Component
+    <TchatReaderMessage message={{ from: "alex", message: "Hello" }} nickname={props.nickname} />
+    <TchatReaderMessage message={{ from: "pierre", message: "Bonjour" }} nickname={props.nickname} />
+    <TchatReaderMessage message={{ from: "Yannick", message: "demat" }} nickname={props.nickname} />
   </div>
 );
 
-TchatReader.propTypes = {};
+TchatReader.propTypes = {
+  nickname: PropTypes.string.isRequired
+};
 
 TchatReader.defaultProps = {};
-
 export default TchatReader;

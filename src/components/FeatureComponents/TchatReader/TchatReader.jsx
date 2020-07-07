f7f029c0560232a68d3ca0ee7abf94c12e2a1a87
alex <desorbaix@free.fr>
2020-07-07 17:35:17 +0200

    mise en pmace du fetch sur le cycle de vie hook effect

--- src/components/FeatureComponents/TchatReader/TchatReader.jsx
+++ src/components/FeatureComponents/TchatReader/TchatReader.jsx
@@ -2,24 +2,42 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 // import  './TchatReader.css';
 import './TchatReader.css'
+import * as moment from 'moment';
 const TchatReader = (props) => {
-  let [messages,setMessages]=useState([{id:0, userId: "alex", message: "degemer mat breizh" },{id:1, userId: "jean-raoul", message: "hello Great brittain" }]);
+  let [messages, setMessages] = useState([{ id: 0, userId: "alex", message: "degemer mat breizh" }, { id: 1, userId: "jean-raoul", message: "hello Great brittain" }]);
+  let [lastReload, setLastReload] = useState(moment('2020-06-01').format('YYYYMMDDHHmmss'));
 
   useEffect(() => {
-   console.log(messages);
-   setMessages([...messages,{id:2,userId:'yannick', message:'te reste t\'il du chouchen?'}])
-  });
-
-  // const messages=[{id:0, userId: "alex", message: "degemer mat breizh" },{id:1, userId: "jean-raoul", message: "hello Great brittain" }]
-  return(
-  <div className="TchatReader">
-    {
-      messages.map((e,i)=>{
-        return  <TchatMessage message={e} nickname={props.nickname} key={`message-${i}`} />
-      })
-    }
-  </div>
-);}
+    console.log(messages);
+    getRestMessages();
+    setInterval(getRestMessages, 2000);
+  }, []);
+  const Moment = moment;
+  const getRestMessages = () => {
+    fetch('http://localhost:4456/messages?_sort=heure&_order=asc&_expand=user&heure_gte=' + lastReload).then(r => r.json()).then(r => {
+      setMessages([...messages, ...r])
+      let lastRevievedMessageDate = lastReload;
+       r.find(e => {
+        if (e.heure > lastRevievedMessageDate) { lastRevievedMessageDate = e.heure }
+      });
+      console.log(lastRevievedMessageDate);
+      setLastReload(lastRevievedMessageDate)
+    })
+  }
+  let formatedTime = moment(lastReload, 'YYYYMMDDHHmmss').format();
+  return (
+    <>
+      last reload : {formatedTime}
+      <div className="TchatReader">
+        {
+          messages.map((e, i) => {
+            return <TchatMessage message={e} nickname={props.nickname} key={`message-${i}`} />
+          })
+        }
+      </div>
+    </>
+  );
+}
 
 TchatReader.propTypes = {
   nickname: PropTypes.string,
@@ -30,9 +48,9 @@ export const TchatMessage = (props) => {
   return (
     <div style={{ textAlign: (isMine ? 'right' : 'left') }}>
       {/* <div className={styles.TchatMessage + (isMine ? " " + styles.mine : "")} >Message</div> */}
-      <div className={"TchatMessage"+(isMine ? " mine":"") } >
-      {props.message.message}
-      <div style={{color:'black',fontSize:'12pt',textAlign:'right',fontStyle:'italic'}}>émis par :{props.message.userId}</div>
+      <div className={"TchatMessage" + (isMine ? " mine" : "")} >
+        {props.message.message}
+        <div style={{ color: 'black', fontSize: '12pt', textAlign: 'right', fontStyle: 'italic' }}>émis par :{props.message.userId}</div>
       </div>
     </div>
   );
